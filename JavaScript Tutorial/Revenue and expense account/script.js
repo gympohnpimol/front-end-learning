8f0fcdaa4ebc5e3ca834574b48ae7d2c6ff89939
gympohnpimol <gympohnpimol@gmail.com>
2021-11-25 22:51:54 +0700

    datalist :construction:

---
+++ JavaScript Tutorial/Revenue and expense account/script.js
@@ -0,0 +1,15 @@
+// element
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+const dataTransaction = [
+    {id:1, text: "candy", amount:-100},
+    {id:2, text: "parttime", amount:+100},
+    {id:3, text: "external-job", amount:+100}
+]
+
