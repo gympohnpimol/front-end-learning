7fc8426825ef146b42be867bcc7c1735b3a36874
gympohnpimol <gympohnpimol@gmail.com>
2022-04-03 14:23:50 +0700

    done :rocket:

--- dark-light mode/script.js
+++ dark-light mode/script.js
@@ -1,14 +1,20 @@
 const switchToggle = document.querySelector('input[type="checkbox"]');
 const toggleIcon = document.getElementById('toggle-icon');
 const nav = document.getElementById('nav');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+
 
 function switchMode(e){
     if(e.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark');
         darkMode();
+        imageSwitchMode('dark');
     }else{
         document.documentElement.setAttribute('data-theme', 'light');
         lightMode();
+        imageSwitchMode('light');
     }
 }
 
@@ -22,6 +28,12 @@ function lightMode(){
     toggleIcon.children[0].textContent = "Light Mode";
     toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
     nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+}
+
+function imageSwitchMode(mode){
+    image1.src=`img/dev_${mode}.svg`
+    image2.src=`img/game_${mode}.svg`
+    image3.src=`img/blockchain_${mode}.svg`
 }
 
 switchToggle.addEventListener('change', switchMode)
